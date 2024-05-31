9f7ea2da50397cdea47b5380fd02169324e25bec
benja <benja.torresr@duocuc.cl>
2024-05-31 19:10:00 -0400

    Login finalizado y se agrego formulario de contacto

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,5 +8,28 @@ const logout = document.querySelector('#logout')
 logout.addEventListener('click', ()=>{
     alert('Hasta pronto!')
     localStorage.removeItem('login_success')
-    window.location.href = 'login.html'
+    window.location.href = 'index.html'
 })
+
+// Obtener elementos
+const btnAbrirModal = document.getElementById('btn-abrir-modal');
+const modal = document.getElementById('modal');
+const btnCerrarModal = document.getElementById('btn-cerrar-modal');
+
+// Evento para abrir el modal
+btnAbrirModal.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+// Evento para cerrar el modal
+btnCerrarModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Cerrar modal al hacer clic fuera de él
+window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+
